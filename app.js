affb297b7ff1db5ba23300ed5f49b7eef6ebb99e
erkankurtaga <erkankurtaga@gmail.com>
2023-10-29 21:13:32 +0300

    Render Fonksiyonu, Veri Listelemek, Aktif Link-continue

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import morgan from "morgan";
-import methodOverride from "method-override";
 import connectDB from "./db.js";
 import pageRoot from "./roots/pageRoot.js";
 import photoRoot from "./roots/photoRoot.js";
@@ -16,9 +14,6 @@ const port = process.env.PORT;
 
 //ejs template engine
 app.set("view engine","ejs");
-
-app.use(morgan("tiny"));
-app.use(methodOverride("_method"));
 
 //static files middleware
 app.use(express.static('public'));
